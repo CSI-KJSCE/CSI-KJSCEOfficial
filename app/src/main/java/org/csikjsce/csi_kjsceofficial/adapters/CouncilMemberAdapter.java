82261e61f3f3a970ccebd0fe42ecd080db40f733
Sarfaraz Ghulam Iraqui <sarfarazghlm@gmail.com>
2017-08-10 11:10:38 +0530

    + CouncilMember adpater and holder

---
+++ app/src/main/java/org/csikjsce/csi_kjsceofficial/adapters/CouncilMemberAdapter.java
@@ -0,0 +1,10 @@
+package org.csikjsce.csi_kjsceofficial.adapters;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by sziraqui on 10/8/17.
+ */
+
+public class CouncilMemberAdapter extends RecyclerView.Adapter {
+}
---
+++ app/src/main/java/org/csikjsce/csi_kjsceofficial/adapters/CouncilMemberHolder.java
@@ -0,0 +1,8 @@
+package org.csikjsce.csi_kjsceofficial.adapters;
+
+/**
+ * Created by sziraqui on 10/8/17.
+ */
+
+public class CouncilMemberHolder {
+}
