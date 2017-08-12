72ea010b748ec31c2f7f6e3f83f6f35db9aaf7f6
Sumit1797 <sumit97poojary@gmail.com>
2017-08-12 23:12:55 +0530

    splashscreen updated

--- app/src/main/java/org/csikjsce/csi_kjsceofficial/SplashscreenActivity.java
+++ app/src/main/java/org/csikjsce/csi_kjsceofficial/SplashscreenActivity.java
@@ -14,34 +14,39 @@ import static java.lang.Thread.sleep;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 
 
-public class SplashscreenActivity extends AppCompatActivity {
+public class SplashscreenActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
 
-        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        final Animation animation_1 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
-        final Animation animation_2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.antirotate);
-        final Animation animation_3 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
+        final ImageView imageView = (ImageView) findViewById(R.id.splash_imageview);
+        final Animation animation_1 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.slide_in_right);
+        final Animation animation_2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.slide_out_left);
 
         imageView.startAnimation(animation_2);
+
+
         animation_2.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
+                imageView.startAnimation(animation_1);
 
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
                 imageView.startAnimation(animation_1);
+                Log.e("splash","Here");
+
             }
 
             @Override
@@ -58,7 +63,7 @@ public class SplashscreenActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                imageView.startAnimation(animation_3);
+
                 finish();
                 Intent i = new Intent(getBaseContext(),MainActivity.class);
                 startActivity(i);
