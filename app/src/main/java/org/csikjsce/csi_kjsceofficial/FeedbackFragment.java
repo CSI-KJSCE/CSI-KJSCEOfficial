7c1af0e06262f336af0bbaf846c108fd22167191
Sumit1797 <sumit97poojary@gmail.com>
2017-08-14 00:00:37 +0530

    About us

--- app/src/main/java/org/csikjsce/csi_kjsceofficial/FeedbackFragment.java
+++ app/src/main/java/org/csikjsce/csi_kjsceofficial/FeedbackFragment.java
@@ -21,16 +21,9 @@ import java.util.ArrayList;
 
 public class FeedbackFragment extends Fragment {
 
-    JSONArray Feeddetails;
-    JSONObject Feeds;
-    Utils FeedJson;
-    ListView liss;
-    ArrayList<FeedbackCard> list= new ArrayList<FeedbackCard>();
+
     private View view ;
-    private final String TAG = "FeedbackListFragment";
-
-
-    @Nullable
+        @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view= inflater.inflate(R.layout.fragment_feedback, container, false);
@@ -40,42 +33,8 @@ public class FeedbackFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        FeedJson =new Utils(this.getContext());
-        Log.e("MainActivity","Eventdetails:"+FeedJson);
-
-        try {
-            Feeds = FeedJson.fetchData("feedback");
-            Log.e("MainActivity","Eventdetails:"+Feeds);
-            Feeddetails = Feeds.getJSONArray("feedback");
-            Log.e("MainActivity","Eventdetails:"+Feeddetails);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-
-        }
 
 
-        String[] Title = new String[Feeddetails.length()];
-        try {
-            for(int i=0;i<Feeddetails.length();i++) {
-
-                Title[i]=Feeddetails.getJSONObject(i).getString("Text");
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        int count=0;
-        for(int i=0;i<Feeddetails.length();i++){
-            FeedbackCard feedcard;
-            feedcard= new FeedbackCard(Title[count]);
-            count++;
-            list.add(feedcard);
-        }
-
-
-       liss =(ListView)view.findViewById(R.id.feedback_listview);
-        Feedbackcard_adapter ada= new Feedbackcard_adapter(getActivity(),list);
-        liss.setAdapter(ada);
     }
 
 }
