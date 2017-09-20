ecc8df1085090983315957a9e9e58dafa217b5de
Sumit1797 <sumit97poojary@gmail.com>
2017-09-20 17:53:01 +0530

    progress bar added

--- app/src/main/java/org/csikjsce/csi_kjsceofficial/HomeFragment.java
+++ app/src/main/java/org/csikjsce/csi_kjsceofficial/HomeFragment.java
@@ -13,6 +13,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -39,6 +40,7 @@ public class HomeFragment extends Fragment {
     RecyclerView eventlists;
     ViewPager viewPager;
     Toolbar toolbar;
+    ProgressBar progress;
 
     SwipeCustomAdapter adapter;
     CircleIndicator indicate;
@@ -59,7 +61,6 @@ public class HomeFragment extends Fragment {
     }
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         dbRef = FirebaseDatabase.getInstance().getReference();
-
         eventsDb = dbRef.child("event");
         view =  inflater.inflate(R.layout.home_fragment,container,false);
         return view;
@@ -72,6 +73,8 @@ public class HomeFragment extends Fragment {
         for(int i=0;i<images.length;i++)
             imgarray.add(images[i]);
         viewPager=(ViewPager)view.findViewById(R.id.View_pager);
+        progress=(ProgressBar)view.findViewById(R.id.center_progressbar);
+
         //indicate=(CircleIndicator) view.findViewById(R.id.indicator);
         //indicate.setViewPager(viewPager);
         adapter = new SwipeCustomAdapter(getActivity(),imgarray);
@@ -79,7 +82,7 @@ public class HomeFragment extends Fragment {
         final Handler handler = new Handler();
         final Runnable Update = new Runnable() {
             public void run() {
-                if (currentPage == images.length) {
+             if (currentPage == images.length) {
                     currentPage = 0;
                 }
                 viewPager.setCurrentItem(currentPage++, true);
@@ -112,6 +115,7 @@ public class HomeFragment extends Fragment {
                 Log.d("HomeFragment","onResume(): uniqueEvents.size():  "+uniqueEvents.size());
                 list.addAll(uniqueEvents);
                 ed.notifyDataSetChanged();
+                progress.setVisibility(View.GONE);
             }
 
             @Override
