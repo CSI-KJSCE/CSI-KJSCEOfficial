8fb148d38e528f5fc810adeaa4df50d0ea009e25
Sarfaraz Ghulam Iraqui <sarfarazghlm@gmail.com>
2017-12-26 16:55:21 +0530

    Eureka fragment load description from firebase

--- app/src/main/java/org/csikjsce/csi_kjsceofficial/EurekaFragment.java
+++ app/src/main/java/org/csikjsce/csi_kjsceofficial/EurekaFragment.java
@@ -3,11 +3,22 @@ package org.csikjsce.csi_kjsceofficial;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * Created by sumit on 5/9/17.
@@ -16,9 +27,9 @@ import android.widget.TextView;
 public class EurekaFragment extends android.support.v4.app.Fragment implements View.OnClickListener {
     private View view;
     TextView eureka_para_tv;
-    String shareAppName;
-    ImageView fbIcon, instaIcon, whatsappIcon, shareIcon;
-
+    String shareAppName, eurekaDescp="desc", registerUrl="url";
+    ImageView fbIcon, instaIcon, whatsappIcon, shareIcon, posterImage;
+    Query query;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -30,6 +41,9 @@ public class EurekaFragment extends android.support.v4.app.Fragment implements V
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        query = FirebaseDatabase.getInstance().getReference("eureka");
+
+        posterImage = view.findViewById(R.id.eureka_poster_iv);
         eureka_para_tv = view.findViewById(R.id.eureka_textview);
         fbIcon = view.findViewById(R.id.facebook_share);
         instaIcon = view.findViewById(R.id.instagram_share);
@@ -40,29 +54,49 @@ public class EurekaFragment extends android.support.v4.app.Fragment implements V
         instaIcon.setOnClickListener(this);
         whatsappIcon.setOnClickListener(this);
         shareIcon.setOnClickListener(this);
+
+        Glide
+                .with(getContext())
+                .load(getContext().getResources().getDrawable(R.drawable.eureka_poster))
+                .into(posterImage);
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                eurekaDescp = dataSnapshot.child("description").getValue(String.class);
+                eureka_para_tv.setText(Html.fromHtml(eurekaDescp));
+                registerUrl = dataSnapshot.child("registeration").getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
         Intent intent;
+        String msg = eurekaDescp+"<br>Register: "+registerUrl;
         switch (v.getId()) {
 
             case R.id.facebook_share:
                 shareAppName = "com.facebook.katana";
-                Utils.onShareClick(getContext(), eureka_para_tv.getText().toString(), shareAppName);
+                Utils.onShareClick(getContext(), msg, shareAppName);
                 break;
             case R.id.instagram_share:
                 shareAppName = "com.instagram.android";
-                Utils.onShareClick(getContext(), eureka_para_tv.getText().toString(), shareAppName);
+                Utils.onShareClick(getContext(), msg, shareAppName);
                 break;
             case R.id.whatsapp_share:
                 shareAppName = "com.whatsapp";
-                Utils.onShareClick(getContext(),eureka_para_tv.getText().toString(), shareAppName);
+                Utils.onShareClick(getContext(), msg, shareAppName);
                 break;
             case R.id.general_share:
                 intent = new Intent();
                 intent.setType("text/plain");
-                intent.putExtra(Intent.EXTRA_TEXT, eureka_para_tv.getText().toString());
+                intent.putExtra(Intent.EXTRA_TEXT, msg);
                 startActivity(intent);
                 break;
         }
