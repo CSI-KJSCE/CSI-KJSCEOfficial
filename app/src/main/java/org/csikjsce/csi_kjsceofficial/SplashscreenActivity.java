eaa95361ebbd06194383da04504cb2e0954300fc
Sarfaraz Ghulam Iraqui <sarfarazghlm@gmail.com>
2018-01-13 20:12:56 +0530

    handle notification from system tray

--- app/src/main/java/org/csikjsce/csi_kjsceofficial/SplashscreenActivity.java
+++ app/src/main/java/org/csikjsce/csi_kjsceofficial/SplashscreenActivity.java
@@ -19,13 +19,24 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import org.csikjsce.csi_kjsceofficial.POJO.Notification;
+
 
 public class SplashscreenActivity extends Activity {
+    public final String TAG = SplashscreenActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        try {
+            if (getIntent().getExtras().getString(getString(R.string.notif_key_id)) != null) {
+                handleNotif();
+            }
+        } catch (NullPointerException npe) {
+           Log.d(TAG,npe.getLocalizedMessage());
+        }
 
         final ImageView imageView = findViewById(R.id.splash_imageview);
         final Animation animation_1 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.fui_slide_in_right);
@@ -73,6 +84,51 @@ public class SplashscreenActivity extends Activity {
 
             }
         });
+    }
+    public void handleNotif(){
+
+        int id = 0;
+        try {
+            id = Integer.parseInt(getIntent().getExtras().getString(getString(R.string.notif_key_id)));
+        } catch (NumberFormatException npe) {
+            Log.d(TAG, npe.getLocalizedMessage());
+            return;
+        }
+        String title = getIntent().getExtras().getString(getString(R.string.notif_key_title));
+        String desc = getIntent().getExtras().getString(getString(R.string.notif_key_desc));
+        String time = getIntent().getExtras().getString(getString(R.string.notif_key_time));
+        String extraUrl = getIntent().getExtras().getString(getString(R.string.notif_key_extra_url));
+
+        int type;
+        try {
+            type = Integer.parseInt(getIntent().getExtras().getString(getString(R.string.notif_key_type)));
+        } catch(NumberFormatException nfe) {
+            type = 0;
+            Log.d(TAG,nfe.getLocalizedMessage());
+        }
+        int eventid = 0;
+        try {
+            eventid = Integer.parseInt(getIntent().getExtras().getString(getString(R.string.notif_key_event_id)));
+        } catch (NumberFormatException nfe) {
+            eventid = -1;
+            Log.d(TAG,nfe.getLocalizedMessage());
+        }
+
+        Notification notification = new Notification(
+                id,
+                time,
+                title,
+                desc,
+                extraUrl,
+                type,
+                eventid,
+                Notification.NOT_READ
+        );
+        DatabaseHelper db = new DatabaseHelper(this);
+        db.insertNotification(notification);
+
+        Intent intent = new Intent(this, NotificationActivity.class);
+        startActivity(intent);
     }
 }
 
